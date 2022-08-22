193ba1b6f0e9c6aeb6be717047469032999abb5b
balu1862 <40159389+balu1862@users.noreply.github.com>
2022-08-22 21:14:08 +0530

    Update brigade.js

--- examples/06-git/.brigade/brigade.js
+++ examples/06-git/.brigade/brigade.js
@@ -1,13 +1,16 @@
-const { events, Job } = require("@brigadecore/brigadier");
+const { events, Job, Container } = require("@brigadecore/brigadier");
 
 events.on("brigade.sh/cli", "exec", async event => {
-  let job = new Job("dood", "docker", event);
-  let keys = Object.keys(job.primaryContainer)
-  console.log(keys);
-  job.primaryContainer.useHostDockerSocket = true;
+  let job = new Job("dind", "docker:stable-dind", event);
+  job.primaryContainer.environment.DOCKER_HOST = "localhost:2375";
   job.primaryContainer.privileged = true;
-  job.primaryContainer.command = ["docker"];
-  job.primaryContainer.arguments = ["ps"];
+  job.primaryContainer.command = ["sh"];
+  job.primaryContainer.arguments = [
+    "-c",
+    // Wait for the Docker daemon to start up
+    // And then pull the image
+    "sleep 20 && docker pull busybox"
+  ];
   await job.run();
 });
 
