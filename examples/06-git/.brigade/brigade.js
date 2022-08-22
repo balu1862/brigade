d1213e565911fed32010ab773cffd3c2e66d2f59
balu1862 <40159389+balu1862@users.noreply.github.com>
2022-08-22 19:26:09 +0530

    Update brigade.js

--- examples/06-git/.brigade/brigade.js
+++ examples/06-git/.brigade/brigade.js
@@ -4,9 +4,21 @@ const { events, Job } = require("@brigadecore/brigadier");
           job1.primaryContainer.command = ["echo"];
           job1.primaryContainer.arguments = ["My first job!"];
           await job1.run();
-          let job2 = new Job("my-second-job", "debian:latest", event);
-          job2.primaryContainer.command = ["echo"];
-          job2.primaryContainer.arguments = ["My second job!"];
+                
+          let job2 = new Job("dind", "docker:stable-dind", event);
+          job.primaryContainer.environment.DOCKER_HOST = "localhost:2375";
+          job.primaryContainer.command = ["sh"];
+          job.primaryContainer.arguments = [
+               "-c",
+               // Wait for the Docker daemon to start up
+               // And then pull the image
+               "sleep 20 && docker pull busybox"
+               ];
+          // Run the Docker daemon in a sidecar container
+          job.sidecarContainers = {
+             "docker": new Container("docker:stable-dind")
+          };
+          job.sidecarContainers.docker.privileged = true
           await job2.run();
         });
         events.process();
