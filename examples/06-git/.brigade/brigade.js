ff539be419ff800136033a42d1b482fee729938d
balu1862 <40159389+balu1862@users.noreply.github.com>
2022-08-22 17:33:09 +0530

    Update brigade.js

--- examples/06-git/.brigade/brigade.js
+++ examples/06-git/.brigade/brigade.js
@@ -1,1 +1,12 @@
-console.log("Hello, Git!");
+const { events, Job } = require("@brigadecore/brigadier");
+        events.on("brigade.sh/cli", "exec", async event => {
+          let job1 = new Job("my-first-job", "debian:latest", event);
+          job1.primaryContainer.command = ["echo"];
+          job1.primaryContainer.arguments = ["My first job!"];
+          await job1.run();
+          let job2 = new Job("my-second-job", "debian:latest", event);
+          job2.primaryContainer.command = ["echo"];
+          job2.primaryContainer.arguments = ["My second job!"];
+          await job2.run();
+        });
+        events.process();
