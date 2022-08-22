601b22856d35d5a96b79594cdc8613faf832fbdb
balu1862 <40159389+balu1862@users.noreply.github.com>
2022-08-22 19:47:18 +0530

    Update brigade.js

--- examples/06-git/.brigade/brigade.js
+++ examples/06-git/.brigade/brigade.js
@@ -2,6 +2,7 @@ const { events, Job, Container } = require("@brigadecore/brigadier");
 
 events.on("brigade.sh/cli", "exec", async event => {
   let job = new Job("dind", "docker:stable-dind", event);
+  job.primaryContainer.docker.privileged = true
   job.primaryContainer.environment.DOCKER_HOST = "localhost:2375";
   job.primaryContainer.command = ["sh"];
   job.primaryContainer.arguments = [
