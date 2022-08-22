3fcc257623bfcce43d7b39a03452c047362ef918
balu1862 <40159389+balu1862@users.noreply.github.com>
2022-08-22 20:17:11 +0530

    Update brigade.js

--- examples/06-git/.brigade/brigade.js
+++ examples/06-git/.brigade/brigade.js
@@ -2,6 +2,8 @@ const { events, Job } = require("@brigadecore/brigadier");
 
 events.on("brigade.sh/cli", "exec", async event => {
   let job = new Job("dood", "docker", event);
+  let keys = Object.keys(job.primaryContainer)
+  console.log(keys);
   job.primaryContainer.useHostDockerSocket = true;
   job.primaryContainer.privileged = true;
   job.primaryContainer.command = ["docker"];
