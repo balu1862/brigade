ea0d6b4c2829f1b8eb80dae77d2644bab312c734
balu1862 <40159389+balu1862@users.noreply.github.com>
2022-08-22 19:43:57 +0530

    Update brigade.js

--- examples/06-git/.brigade/brigade.js
+++ examples/06-git/.brigade/brigade.js
@@ -3,13 +3,14 @@ const { events, Job, Container } = require("@brigadecore/brigadier");
 events.on("brigade.sh/cli", "exec", async event => {
   let job = new Job("dind", "docker:stable-dind", event);
   job.primaryContainer.environment.DOCKER_HOST = "localhost:2375";
-  //job.primaryContainer.command = ["sh"];
-  //job.primaryContainer.arguments = [
-  //  "-c",
+  job.primaryContainer.command = ["sh"];
+  job.primaryContainer.arguments = [
+    "-c",
     // Wait for the Docker daemon to start up
     // And then pull the image
-  // "sleep 20 && docker pull busybox"
-  //];
+    //"sleep 20 && docker pull busybox"
+    "sleep 1"
+  ];
 
   // Run the Docker daemon in a sidecar container
   job.sidecarContainers = {
